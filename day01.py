3f0ab02fe9838f32fa280946308401b2df94c419
DESKTOP-DP2C5J4\USER <siko2828@naver.com>
2025-02-24 11:34:39 +0900

    v0.2 pandas

--- day01.py
+++ day01.py
@@ -1,4 +1,11 @@
 import numpy as np
+import pandas as pd
 
-v=np.array([1,3,-9,2])
-print(v,v.ndim)
+
+#dataframe은 2차원의 표형태,serial은 1차원형태(예를들어, 열)
+df = pd.DataFrame(
+{"a" : [4, 5, 6],
+      "b" : [7, 8, 9],
+      "c" : [10, 11, 12]},
+      index = [1, 2, 3]) #index:row명, dict의 key:column명
+print(df)
