148e55d541688c1a03a3ffae3ab9ca85c019b909
DESKTOP-DP2C5J4\USER <siko2828@naver.com>
2025-02-24 11:37:56 +0900

    v0.3 pandas_2차원

--- day01.py
+++ day01.py
@@ -9,3 +9,11 @@ df = pd.DataFrame(
       "c" : [10, 11, 12]},
       index = [1, 2, 3]) #index:row명, dict의 key:column명
 print(df)
+
+print('------------------------------')#위의 df와 아래의 df는 차이점은 데이터가 열로 쭉들어가냐, 행으로 쭉 들어가냐의 차이점이다.
+df_=pd.DataFrame(
+      [[4,7,10],
+       [5,8,11],
+       [6,9,12]]
+)
+print(df_)
